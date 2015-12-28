4ef69864598386859f4851ae9c46a402b6394f03
Oren Zitoun <sorenzi@yahoo.com>
2015-12-27 23:37:51 -0800

    handling keyboard in text vc

--- JewList/FreeTextViewController.m
+++ JewList/FreeTextViewController.m
@@ -16,10 +16,26 @@
 @property (weak, nonatomic) IBOutlet UILabel *questionTitleLabel;
 @property (weak, nonatomic) IBOutlet UIPlaceHolderTextView *textView;
 @property (weak, nonatomic) IBOutlet UIButton *nextButton;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *nextButtonBottomConstraint;
 
 @end
 
 @implementation FreeTextViewController
+
+
+- (void)registerNotification {
+}
+
+-(void)viewWillAppear:(BOOL)animated {
+    [[NSNotificationCenter defaultCenter] addObserver:self  selector:@selector(keyboardWillShowNotification:) name:UIKeyboardWillShowNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self  selector:@selector(keyboardWillHideNotification:) name:UIKeyboardWillHideNotification object:nil];
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardWillShowNotification object:nil];
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardWillHideNotification object:nil];
+    [self.textView resignFirstResponder];
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -39,7 +55,6 @@
 
 - (void)didMoveToParentViewController:(UIViewController *)parent {
     if (!parent) {
-        NSLog(@"back pressed");
         if (self.saveOnBackButton) {
             if ([self.delegate respondsToSelector:@selector(freeTextControllerDidChooseText:text:)]) {
                 [self.delegate freeTextControllerDidChooseText:self text:self.textView.text];
@@ -53,6 +68,36 @@
     if ([self.delegate respondsToSelector:@selector(freeTextControllerDidChooseText:text:)]) {
         [self.delegate freeTextControllerDidChooseText:self text:self.textView.text];
     }
+}
+
+#pragma mark - Keyboard notifications
+
+- (void)keyboardWillShowNotification:(NSNotification *)notification
+{
+    [self animateScrollViewWithKeyboardNotification:notification];
+}
+
+- (void)keyboardWillHideNotification:(NSNotification *)notification
+{
+    [self animateScrollViewWithKeyboardNotification:notification];
+}
+
+- (void)animateScrollViewWithKeyboardNotification:(NSNotification *)notification
+{
+    CGRect keyboardRect = [[notification.userInfo objectForKey:UIKeyboardFrameEndUserInfoKey] CGRectValue];
+    CGFloat keyboardTop = CGRectGetMinY([self.view convertRect:keyboardRect fromView:nil]);
+    CGFloat bottomInset = MAX(0, self.view.height - keyboardTop);
+  
+    NSTimeInterval duration = [[notification.userInfo objectForKey:UIKeyboardAnimationDurationUserInfoKey] doubleValue];
+    UIViewAnimationCurve curve = [[notification.userInfo objectForKey:UIKeyboardAnimationCurveUserInfoKey] integerValue];
+    UIViewAnimationOptions options = UIViewAnimationOptionBeginFromCurrentState | (curve << 16);
+    [UIView animateWithDuration:duration
+                          delay:0.0
+                        options:options
+                     animations:^{
+                         self.nextButtonBottomConstraint.constant = bottomInset;
+                     }
+                     completion:nil];
 }
 
 @end
